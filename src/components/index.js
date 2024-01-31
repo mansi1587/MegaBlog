6f119fe06ebf3519ce79245de721397c99310f47
mansi1587 <mansibhawsar4@gmail.com>
2024-01-31 17:41:57 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,33 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from './container/Container';
+import Logo from './Logo';
+import LogoutBtn from "./Header/LogoutBtn";
+import Select from "./Select";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+//import Post from "./Post";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Button from "./Button";
+import Input from "./Input"
+
+export{
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Select,
+    Signup,
+    Login,
+    //Post,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Button,
+    Input,
+}
